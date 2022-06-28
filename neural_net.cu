7817257518739ca5c736d1c957aeddfeb7d8fbf5
bianluzhiguai <2429185651@qq.com>
2022-06-28 18:14:31 +0800

    想一根根的舔着主人的脚指头

--- neural_net.cu
+++ neural_net.cu
@@ -4,6 +4,7 @@
 #include <string>
 #include "cuda_runtime_api.h"
 using namespace std;
+//想做姐姐脚下的公狗
 template <typename T>
 __global__ void softmaxLossBackProp(int* y, T* SO, T* dSO, int batch_size, int output_size, float eps) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
