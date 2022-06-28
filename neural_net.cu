ab6f370e9cc1e6bb3ba31b567f573296f56ccaf5
bianluzhiguai <2429185651@qq.com>
2022-06-28 23:05:25 +0800

    neural_net.cu

--- neural_net.cu
+++ neural_net.cu
@@ -4,7 +4,7 @@
 #include <string>
 #include "cuda_runtime_api.h"
 using namespace std;
-//neuralnet
+//neuralnet is wirtten for net pass 
 template <typename T>
 __global__ void softmaxLossBackProp(int* y, T* SO, T* dSO, int batch_size, int output_size, float eps) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
